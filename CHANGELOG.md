39e6020bd7e55426d8014bcf464671940db77eba
Kurt Duyck <kurt.duyck@vives.be>
2020-12-16 15:12:37 +0100

    Clear build/ on npm run build
    	modified:   CHANGELOG.md
    	modified:   package.json
    	modified:   snowpack.config.js

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.2.2] - 2020-12-16
+### Changed
+- Clear build dir on every build
+
 ## [0.2.1] - 2020-12-15
 ### Added
 - Snowpack config
--- snowpack.config.js
+++ snowpack.config.js
@@ -1,5 +1,11 @@
 // Snowpack Configuration File 
 module.exports = {
+  buildOptions: {
+    /* ? ... */
+    // buildOptions.clean | boolean | Default: false#
+    // Set to true if Snowpack should erase the build folder before each build.
+    "clean": true
+  },
   mount: {
     "src": "/"
   }
