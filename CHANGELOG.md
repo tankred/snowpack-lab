7e44255bdd8a896fc28c5a7dd40b7c900851348d
Kurt Duyck <kurt.duyck@vives.be>
2021-04-02 10:25:34 +0200

    Minify html and js
    	modified:   CHANGELOG.md
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   snowpack.config.js
    	modified:   src/index.html

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.3.1] - 2021-04-02
+### Changed
+- Minify Js and html
+
 ## [Released]
 
 ## [0.3.0] - 2021-04-01
--- snowpack.config.js
+++ snowpack.config.js
@@ -4,15 +4,44 @@ module.exports = {
   optimize: {
     bundle: true,
     minify: true,
-    target: 'es2018'
+    target: 'es2020'
   },
   mount: {
     src: '/'
   },
   exclude: ['**/*~'],
+//   plugins: [
+//     ["@snowpack/plugin-optimize", { 
+//       minifyHTML: true
+//     }]
+//   ]
   plugins: [
-    ["@snowpack/plugin-optimize", { 
-      minifyHTML: true
-    }]
-  ]
+    [
+      'snowpack-plugin-minify-html',
+      {
+        /**
+         * @see Plugin Options below
+         */
+        htmlMinifierOptions: {
+          sortAttributes: true,
+          removeComments: true,
+        },
+      },
+    ],
+    [
+      'snowpack-plugin-terser',
+      {
+        /**
+         * @see Plugin Options below
+         */
+        terserOptions: {
+          compress: {
+            arguments: true,
+            passes: 2,
+            unsafe_arrows: true,
+          },
+        },
+      },
+    ],
+  ],
 }
